061d5861d20e0a19279d70985b7c99ce4f496c0f
Ubuntu <ubuntu@ip-172-31-0-18.ec2.internal>
2024-10-25 16:45:07 +0000

    code added

--- main.tf
+++ main.tf
@@ -1,1 +1,174 @@
-hi 
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "myvpc"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "public1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_security_group" "public_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "private_sg" {
+  name        = "private_sg"
+  description = "Allow SSH from within VPC"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "private_sg"
+  }
+}
+resource "aws_instance" "public1" {
+  ami           = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public1.id
+  vpc_security_group_ids = [aws_security_group.public_sg.id]
+   tags = {
+    Name = "PublicInstance1"
+  }
+}
+
+resource "aws_instance" "public2" {
+  ami           = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public2.id
+  vpc_security_group_ids = [aws_security_group.public_sg.id]
+   tags = {
+    Name = "PublicInstance2"
+  }
+}
+
+resource "aws_instance" "private" {
+  ami           = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private.id
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
+   tags = {
+    Name = "Privateinstance"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
